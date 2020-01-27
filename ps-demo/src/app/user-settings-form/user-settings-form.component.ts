7c84069b0187448dc70629fa97b18ae9a8ef01ea
thiagodevx <thiago.dev.x@gmail.com>
2020-01-27 13:17:22 -0300

    creation of the UserSettings interface

--- ps-demo/src/app/user-settings-form/user-settings-form.component.ts
+++ ps-demo/src/app/user-settings-form/user-settings-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserSettings } from '../data/user-settings';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -7,6 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserSettingsFormComponent implements OnInit {
 
+  userSettings: UserSettings = {
+    name: 'Milton',
+    emailOffers: true,
+    interfaceStyle: 'dark',
+    subscriptionType: 'Annual',
+    notes: 'here are some notes...'
+  };
+  
   constructor() { }
 
   ngOnInit() {
